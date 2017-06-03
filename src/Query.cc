9d0dfd47fc912248ea1a78cb5060423633583f7e
Ricardo Constantino <wiiaboo@gmail.com>
2017-06-03 18:15:36 +0100

    add implementation of timersub for mingw

--- src/Query.cc
+++ src/Query.cc
@@ -39,6 +39,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+#ifdef __MINGW32__
+#define timersub(a, b, result) \
+    do { \
+        (result)->tv_sec = (a)->tv_sec - (b)->tv_sec; \
+        (result)->tv_usec = (a)->tv_usec - (b)->tv_usec; \
+        if ((result)->tv_usec < 0) { \
+            --(result)->tv_sec; \
+            (result)->tv_usec += 1000000L; \
+        } \
+    } while (0)
+#endif
 
 #include <ne_uri.h>
 
